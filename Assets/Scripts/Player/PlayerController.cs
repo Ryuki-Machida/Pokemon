d51abf3d71cf206054d051892146eb9f3c45b86f
RYUKI <ryukiarashi@icloud.com>
2021-09-13 21:35:46 +0900

    プレイヤーの動き

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float m_moveSpeed;
+
+    private bool m_isMoving;
+    private Vector2 input;
+
+    void Update()
+    {
+        if (!m_isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            //斜め移動が出来なくなる
+            if (input.x != 0)
+            {
+                input.y = 0;
+            }
+
+            if (input != Vector2.zero)
+            {
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                StartCoroutine(Move(targetPos));
+            }
+        }
+    }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        m_isMoving = true;
+
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, m_moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+
+        m_isMoving = false;
+    }
+}
